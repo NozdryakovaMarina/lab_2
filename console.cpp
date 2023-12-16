292b36481f2315c63bb4d808d00f31727fb0448a
Marina <nozdryakovamarina94@gmail.com>
2023-12-16 22:35:20 +0400

    merge, insert, dupl

--- console.cpp
+++ console.cpp
@@ -4,4 +4,64 @@ using namespace list;
 
 int main() {
 
+	LinkedList<int> l;
+	/*Node<int>* x = new Node(1);
+	Node<int>* y = new Node(-2);
+	Node<int>* z = new Node(3);*/
+
+	LinkedList<int> l2;
+	/*Node<int>* a = new Node(4);
+	Node<int>* b = new Node(6);
+	Node<int>* c = new Node(-7);*/
+
+	LinkedList<int> l4;
+	/*Node<int>* e = new Node(10);
+	Node<int>* s = new Node(41);
+	Node<int>* q = new Node(-77);*/
+
+	l.push_head(1);
+	l.push_tail(3);
+	l.push_head(-2);
+
+	/*cout << l;*/
+
+	l2.push_head(4);
+	l2.push_tail(6);
+	l2.push_head(-77);
+
+	l4.push_head(10);
+	l4.push_tail(11);
+	l4.push_head(-77);
+
+	//cout << l4 << endl;
+
+	/*cout << l << endl;
+	cout << l2 << endl;
+	cout << l4 << endl;
+
+	LinkedList<int> l5;
+	l5 = l2;
+
+	cout << l5 << endl;
+
+	cout << l5.get_elem() << endl;
+
+	l.delete_node(3);
+
+	cout << l << endl;
+
+	l2.push_tail(l);
+	l5.push_head(l4);
+
+	cout << l5 << endl;
+	cout << l2 << endl;*/
+
+	LinkedList<int> l3;
+	l3 = merge(l4, l2);
+	removeDuplicates(l3);
+	cout << l3 << endl;
+
+	/*cout << l3[0];*/
+	
+	return 0;
 }
--- linked_list.cpp
+++ linked_list.cpp
@@ -2,144 +2,359 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <random>
 
 using namespace std;
 
 namespace list {
-	template<typename T>
-	struct Node {
-		T _val;
-		Node<T>* _next;
-		Node<T>* _prev;
-		Node() : _val(0), _next(nullptr), _prev(nullptr) {}
-		Node(T val, Node<T>* next, Node<T>* prev): _val(val), _next(next), _prev(prev) {}
-	};
+    template <typename T>
+    struct Node {
+        T _data;
+        Node<T>* _prev;
+        Node<T>* _next;
 
-	template<typename T>
-	class LinkedList {
-		Node<T>* _head;
-		Node<T>* _tail;
-	public:
-		LinkedList(): _head(nullptr), _tail(nullptr) {}
+        Node(): _data(0), _prev(nullptr), _next(nullptr) {}
+        Node(T d) : _data(d), _prev(nullptr), _next(nullptr) {}
+    };
 
-		LinkedList(const LinkedList<T>& l) {
-			_head = nullptr;
-			_tail = nullptr;
-			Node<T>* tmp = l._head;
-			while (tmp != l._head) {
-				Node<T>* node = new Node<T>(tmp->_val);
-				push_tail(node);
-				tmp = tmp->_next;
-			}
-		}
+    template<typename T>
+    ostream& operator<<(ostream& os, Node<T>* _node) {
+        os << _node->_data << "|" << endl;
+        return os;
+    }
 
-		void swap(LinkedList<T>& l) {
-			std::swap(_head, l._head);
-			std::swap(_tail, l._tail);
-		}
+    template <typename T>
+    class LinkedList {
+    private:
+        Node<T>* _head;
+        Node<T>* _tail; 
 
-		LinkedList<T>& operator=(LinkedList<T> l) {
-			swap(l);
-			return *this;
-		}
+    public:
+        // Default constructor
+        LinkedList() : _head(nullptr), _tail(nullptr) {}
 
-		~LinkedList() {
-			while (_head != _tail) {
-				Node<T>* tmp = _head;
-				_head = _head->prev;
-				delete tmp;
-			}
-			delete _tail;
-		}
+        Node<T>* get_head() const { return _head; }
+        Node<T>* get_tail() const { return _tail; }
 
-		void push_head(Node<T>* h) {
-			if (_head != nullptr) {
-				_head->_prev = h;
-				_tail->_next = h;
-			}
-			if (_head == nullptr) 
-				_tail = h;
-			h->_next = _head;
-			h->_prev = _tail;
-			_head = h;
-		}
+        // Конструктор, заполняющий список случайными значениями
+        LinkedList(size_t n, T a, T b) : _head(nullptr), _tail(nullptr){
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<T> dis(a, b);
+            for (size_t i = 0; i < n; ++i) {
+                push_tail(dis(gen));
+            }
+        }
 
-		void push_head(LinkedList<T>& l) {
-			if (l._head == nullptr)
-				throw invalid_argument("The list is empty.");
-			if (_head == nullptr) {
-				_head = l._head;
-				_tail = l._tail;
-			}
-			   
-		}
+        // Деструктор
+        ~LinkedList() = default;
+        //~LinkedList() {
+        //    while (_head != _tail) {
+        //        Node<T>* tmp = _head;
+        //        _head = _head->_prev;
+        //        delete tmp;
+        //    }
+        //    delete _tail;
+        //}
 
-		void push_tail(Node<T>* t) {
-			if (_tail != nullptr) {
-				_tail->_prev = t;
-				_head->_next = t;
-			}
-			if (_tail == nullptr)
-				_head = t;
-			t->_prev = _head;
-			t->_next = _tail;
-			_tail = t;
-		}
+        // Конструктор копирования
+        LinkedList(const LinkedList<T>& l) : _head(nullptr), _tail(nullptr){
+            Node<T>* cur = l._head;
+            while (cur != nullptr) {
+               /* Node<T>* tmp = new Node(cur->_data);*/
+                push_tail(cur->_data);
+                cur = cur->_next;
+            }
+        }
 
-		void push_tail(LinkedList<T>& l) {
-			if (l.pop_head == nullptr)
-				throw invalid_argument("The list is empty.");
-			if (_tail == nullptr) {
-				_head = l._head;
-				_tail = l._tail;
-			}
+        void swap(LinkedList<T>& l) {
+            std::swap(_head, l._head);
+        }
 
-		}
+        LinkedList<T>& operator=(const LinkedList<T>& other) {
+            if (other._head) {
+                Node<T>* tmp = other._head;
+                do {
+                   /* Node<T>* cur = new Node(tmp->_data);*/
+                    push_tail(tmp->_data);
+                    tmp = tmp->_next;
+                } while (tmp != other._head);
+            }
+            return *this;
+        }
 
-		void pop_head() {
-			if (_head == nullptr)
-				throw invalid_argument("The list is empty.");
+        // Добавление элемента в конец списка
+        void push_tail(T d) {
+            Node<T>* tmp = new Node(d);
+            if (_tail == nullptr) {
+                _head = tmp;
+                _tail = tmp;
+                _tail->_next = _head;
+                _head->_prev = _tail;
+            }
+            else {
+                _tail->_next = tmp;
+                tmp->_prev = _tail;
+                _tail = tmp;
+                _tail->_next = _head;
+                _head->_prev = _tail;
+            } 
+        }
 
-			Node<T>* tmp = _head->_next;
-			_head->_prev = _tail;
-			_tail->_next = _head;
-			delete tmp;
-		}
+        // Добавление другого списка в конец списка
+        void push_tail(const LinkedList& l) {
+            if (l._tail == nullptr)
+                throw std::invalid_argument("LinkedList::The list is empty");
+            else if (_tail == nullptr) {
+                _head = l._head;
+                _tail = l._tail;
+            }
+            else {
+                l._tail->_next = _head;
+                _head->_prev = l._tail;
+                l._head->_prev = _tail;
+                _tail->_next = l._head;
+                _tail = l._tail;
+            }
+        }
 
-		void pop_tail() {
-			if (_tail == nullptr)
-				throw invalid_argument("The list is empty.");
+        // Добавление элемента в начало списка
+        void push_head(T d) {
+            Node<T>* tmp = new Node(d);
+            if (_head == nullptr) {
+                _head = tmp;
+                _tail = tmp;
+                _tail->_next = _head;
+                _head->_prev = _tail;
+            }
+            else {
+                tmp->_next = _head;
+                _head->_prev = tmp;
+                _head = tmp;
+                _head->_prev = _tail;
+                _tail->_next = _head;
+            } 
+        }
 
-			Node<T>* tmp = _tail->_prev;
-			_head->_prev = _tail;
-			_tail->_next = _head;
-			delete tmp;
-		}
+        // Добавление списка в начало списка
+        void push_head(const LinkedList& _list) {
+            if (_list._head == nullptr)
+                throw std::invalid_argument("LinkedList::The list is empty");
+            else if (_head == nullptr) {
+                _head = _list._head;
+                _tail = _list._tail;
+            }
+            else {
+                _list._tail->_next = _head;
+                _head->_prev = _list._tail;
+                _list._head->_prev = _tail;
+                _tail->_next = _list._head;
+                _head = _list._head;
+            }
+        }
 
-		void delete_node(T val) {
-			Node<T> tmp = _head;
+        // Удаление элемента из начала списка
+        void pop_head() {
+            if (_head == NULL)
+                throw std::invalid_argument("LinkedList::The list is empty");
+            else {
+                Node<T>* cur = _head;
+                _head = _head->_next;
+                _head->_prev = _tail;
+                _tail->_next = _head;
+                delete cur;
+            }
+        }
 
-		}
+        // Удаление элемента из конца списка
+        void pop_tail() {
+            if (_tail == NULL)
+                throw std::invalid_argument("LinkedList::The list is empty");
+            else {
+                Node<T>* cur = _tail;
+                _tail = _tail->_prev;
+                _head->_prev = _tail;
+                _tail->_next = _head;
+                delete cur;
+            }
+        }
 
-		int get_size(Node<T>* h) const{
-			int count = 0;
-			Node<T>* tmp = h;
-			while (tmp != h) {
-				count++;
-				tmp = tmp->_next;
-			}
-			return count;
-		}
-		
-		Node<T>* operator[](int ind) const {
-			if (ind < 0 || ind > = get_size(_head))
-				throw std::out_of_range("Index is out of range.");
-			else {
-				Node<T>* tmp = _head;
-				for (int i = 0; i < ind; i++)
-					tmp = tmp->next;
-				return tmp;
-			}
-		}
-	};
+        // Удаление всех элементов с информационным полем, равным переданному
+        void delete_node(T d) {
+            if (_head == nullptr)
+                throw std::invalid_argument("LinkedList::The list is empty");
+            else {
+                Node<T>* cur = _head;
+                do {
+                    if (d == cur->_data) {
+                        if (cur == _head) {
+                            cur = cur->_next;
+                            this->pop_head();
+                        }
+                        else if (cur == _tail) {
+                            cur = cur->_next;
+                            this->pop_tail();
+                        }
+                        else {
+                            Node<T>* ptr = cur;
+                            cur = cur->_next;
+                            cur->_prev = ptr->_prev;
+                            ptr->_prev->_next = cur;
+                            delete ptr;
+                        }
+                    }
+                    else {
+                        cur = cur->_next;
+                    }
+                } while (cur != _head);
+            }
+        }
+
+        // Операция доступа по индексу для чтения
+        T& operator[](size_t index) const {
+            if (index < get_elem()) {
+                Node<T>* curr = _head;
+                for (size_t i = 0; i < index; ++i) {
+                    curr = curr->_next;
+                }
+                return curr->_data;
+            }
+            else {
+                throw std::out_of_range("Index out of range");
+            }
+        }
+
+        // Операция доступа по индексу для записи
+        T& operator[](size_t index) {
+            if (index < get_elem()) {
+                Node<T>* curr = _head;
+                for (size_t i = 0; i < index; ++i) {
+                    curr = curr->_next;
+                }
+                return curr->_data;
+            }
+            else {
+                throw std::out_of_range("Index out of range");
+            }
+        }
+
+        int get_elem() const {
+            if (_head == nullptr)
+                return 0;
+            int len = 1;
+            Node<T>* node = _head->_next;
+            while (node != _head) {
+                len += 1;
+                node = node->_next;
+            }
+            return len;
+        }
+
+        void insert(T val) {
+            Node<T>* newNode = new Node(val);
+            if (_head == nullptr) {
+                _head = newNode;
+                _tail = newNode;
+                _head->_next = _tail;
+                _tail->_prev = _head;
+            }
+            else {
+                Node<T>* temp = _head;
+                while (temp->_next != _tail) {
+                    if (temp->_data <= val && temp->_next->_data >= val) {
+                        break;
+                    }
+                    temp = temp->_next;
+                }
+                if ((temp == _head && temp->_data > val) || temp->_prev->_data > val) {
+                    newNode->_next = _head;
+                    _head->_prev = newNode;
+                    _head = newNode;
+                }
+                else if ((temp == _tail && temp->_data < val) || temp->_next->_data < val) {
+                    newNode->_prev = _tail;
+                    _tail->_next = newNode;
+                    _tail = newNode;
+                }
+                else {
+                    newNode->_next = temp->_next;
+                    newNode->_prev = temp;
+                    temp->_next->_prev = newNode;
+                    temp->_next = newNode;
+                }
+            }
+        }
+  };
+
+  template<typename T>
+  ostream& operator<<(ostream& os, const LinkedList<T>& l) {
+      os << l.get_elem() << endl;
+      return os;
+  }
+
+  template<typename T>
+  ostream& operator<<(ostream& os, LinkedList<T>& l) {
+      Node<T>* cur = l.get_head();
+      do {
+          os << cur->_data << " ";
+          cur = cur->_next;
+      } while (cur != l.get_head());
+      return os;
+  }
+
+  template<typename T>
+  void removeDuplicates(LinkedList<T>& l) {
+      Node<T>* current = l.get_head();
+      Node<T>* index = nullptr;
+      Node<T>* temp = nullptr;
+
+      if (l.get_head() == nullptr) {
+          return;
+      }
+      else {
+          do {
+              temp = current;
+              index = current->_next;
+
+              while (index != l.get_head()) {
+                  if (index->_data == current->_data) {
+                      temp->_next = index->_next;
+                      delete index;
+                      index = temp->_next;
+                  }
+                  else {
+                      temp = index;
+                      index = index->_next;
+                  }
+              }
+              current = current->_next;
+          } while (current != l.get_head());
+      }
+  }
+
+
+  template<typename T>
+  LinkedList<T> merge(LinkedList<T>& l1, LinkedList<T>& l2) {
+      LinkedList<T> mergedList;
+      Node<T>* temp1 = l1.get_head();
+      while (temp1 != nullptr) {
+          mergedList.insert(temp1->_data);
+          temp1 = temp1->_next;
+          if (temp1 == l1.get_head()) {
+              break;
+          }
+      }
+
+      Node<T>* temp2 = l2.get_head();
+      while (temp2 != nullptr) {
+          mergedList.insert(temp2->_data);
+          temp2 = temp2->_next;
+          if (temp2 == l2.get_head()) {
+              break;
+          }
+      }
+
+      return mergedList;
+  }
+ 
 }
